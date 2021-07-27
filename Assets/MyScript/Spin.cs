84a2c4dc1abbb9cdef81061234e6f8760f0a1c17
as <>
2021-07-27 17:29:36 +0900

    Shader 波の表現

---
+++ Assets/MyScript/Spin.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spin : MonoBehaviour
+{
+    public Vector3 vect;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.Rotate(vect * Time.deltaTime);
+    }
+}
