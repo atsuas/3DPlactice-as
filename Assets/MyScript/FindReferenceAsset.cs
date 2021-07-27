465e10afee7bfbe637b557e84ceef969f154f891
as <>
2021-07-27 09:03:49 +0900

    右クリックメニューの追加、ウィンドウの表示をする

---
+++ Assets/MyScript/FindReferenceAsset.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FindReferenceAsset : EditorWindow
+{
+    [MenuItem("Assets/参照を探す",false)]
+    public static void FindAssets()
+    {
+        Debug.Log("Start Find Reference Asset!");
+        GetWindow<FindReferenceAsset>();
+    }
+}
